8048603f2477e9f85871d85132a480be425d346e
mohit <patelmohit672@gmail.com>
2024-06-27 18:07:55 +0530

    counter commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
+  let [counter, setCounter] = useState(0)
+  
+
+  const addValue = () =>{
+    setCounter(counter + 1)
+  }
+  const removeValue = () =>{
+    setCounter(counter - 1)
+  }
+
   return (
     <>
       <h1>React One</h1>
-      
+      <h3>Counter: {counter}</h3>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={removeValue}>Remove value</button>
     </>
   )
 }
